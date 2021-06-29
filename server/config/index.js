4660e37718e512d05422907aaa8d551cd6ddb7d3
burakkaanerce <burak.ercedogan@gmail.com>
2021-06-29 10:59:51 +0300

    safety-first but in github

--- server/config/index.js
+++ server/config/index.js
@@ -1,4 +1,4 @@
 export default {
-  DB_URI: 'mongodb+srv://burakkaanerce:123luna123@cluster0.snhqy.mongodb.net/leaderboard?retryWrites=true&w=majority',
-  DB_NAME: 'leaderboard',
+  DB_URI: process.env.DB_URI,
+  DB_NAME: process.env.DB_NAME,
 }
